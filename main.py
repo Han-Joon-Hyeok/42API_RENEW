7450786811bf14b8e1a671751b72587421f86785
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-04-07 23:20:46 +0900

    ✨ feat: Add getting env variables from .env file

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from fortytwo_auto_api import fortytwo_auto_keys
 import secret
 
 fortytwo = fortytwo_auto_keys(
-	login=secret.LOGIN_42,
-	password=secret.PASSWORD_42,
-	app_url=secret.APP_URL,
-	otp_secret=secret.OTP_SECRET_42,
+	login=secret.config["LOGIN_42"],
+	password=secret.config["PASSWORD_42"],
+	app_url=secret.config["APP_URL"],
+	otp_secret=secret.config["OTP_SECRET_42"],
 )
 
 fortytwo.auto()
 
+API_SECRET_SAVED_FILE = secret.config["API_SECRET_FILE_PATH"] + secret.config["API_SECRET_FILE_NAME"]
+
 api_keys = fortytwo.keys
-with open(".env.api", "w+") as f:
+with open(API_SECRET_SAVED_FILE, "w+") as f:
 	f.write(f"PAYLOAD_CLIENT_ID={api_keys['uid']}\n")
 	f.write(f"PAYLOAD_CLIENT_SECRET={api_keys['secret']}\n")
--- secret.py
+++ secret.py
@@ -1,4 +1,3 @@
-LOGIN_42="<42 intra id>"
-PASSWORD_42="<42 intra password>"
-APP_URL="<42 intra api application link>"
-OTP_SECRET_42="<OTP_secret>" # Or None
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
