cc2902ef05b0a0c1541324800294995653a57b01
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-04-08 00:02:57 +0900

    ✨ feat: Add saving exception traceback

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import logging
+import traceback
+
 from fortytwo_auto_api import fortytwo_auto_keys
 import secret
+import log
+import env
 
 fortytwo = fortytwo_auto_keys(
 	login=secret.config["LOGIN_42"],
@@ -8,11 +13,15 @@ fortytwo = fortytwo_auto_keys(
 	otp_secret=secret.config["OTP_SECRET_42"],
 )
 
-fortytwo.auto()
+def main():
+	try:
+		log.setup_logger()
+		fortytwo.auto()
+		env.save_secret_values_in_env_file()
+		
+	except Exception as e:
+		logging.error(e)
+		logging.error(traceback.format_exc())
 
-API_SECRET_SAVED_FILE = secret.config["API_SECRET_FILE_PATH"] + secret.config["API_SECRET_FILE_NAME"]
-
-api_keys = fortytwo.keys
-with open(API_SECRET_SAVED_FILE, "w+") as f:
-	f.write(f"PAYLOAD_CLIENT_ID={api_keys['uid']}\n")
-	f.write(f"PAYLOAD_CLIENT_SECRET={api_keys['secret']}\n")
+if __name__ == "__main__":
+	main()
